39273a2d7417fc885c8cc4fbbf09e2c84ff6e034
Julien Dupont <julien.dupont2018@gmail.com>
2021-01-22 16:36:18 +0100

    update readme and files

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ node index.js
 # Launch test
 
 npm test 
+
+# Résultats des tests 
+
+```
+ √ should return all colors
+  √ should return bad request
+  √ should add new color
+  √ should return new color list Request
+
+  4 passing (132ms)
+```
--- test/test.js
+++ test/test.js
@@ -20,7 +20,63 @@ const payloadColor = () => {
   nextCursor++;
   return { 'color': nextColor }
 }
+const newColor = payloadColor();
+
 const getCurrentCulor = () => {
   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
 }
 // <-- FIN
+
+it('should return all colors', function (done) {
+  const expectedColors = ['RED', 'GREEN', 'BLUE'];
+  chai.request(app)
+    .get('/colors')
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.an.instanceof(Object);
+      expect(res.body.results).to.be.an('array');
+      expect(res.body.results).to.eql(expectedColors)
+      console.log(res.body.results, expectedColors)
+      done();
+    });
+});
+
+
+it('should return bad request', function (done) {
+  chai.request(app)
+    .get('/colo')
+    .end((err, res) => {
+      expect(res).to.have.status(404)
+      done();
+    });
+});
+
+it('should add new color', function (done) {
+  chai.request(app)
+    .post('/colors')
+    .send(newColor)
+    .then((res) => {
+      expect(res).to.have.status(201);
+      expect(res.body).is.an.instanceof(Object);
+      expect(res.body.results).to.be.an('array');
+      expect(res.body.results).to.contain(newColor.color)
+      done();
+    })
+});
+
+it('should return new color list Request', function (done) {
+  chai.request(app)
+    .get('/colors')
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.an.instanceof(Object);
+      expect(res.body.results).to.be.an('array');
+      expect(res.body.results).to.contain(newColor.color)
+      done();
+    });
+});
+
+
+
